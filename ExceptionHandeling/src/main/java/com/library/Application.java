197b39e9422e0376a5c172c95e4ef6935af8853a
udeshiperera <budeshimperera@gmail.com>
2020-12-17 19:38:11 +0530

    code changed in exception.

--- ExceptionHandeling/src/main/java/com/library/Application.java
+++ ExceptionHandeling/src/main/java/com/library/Application.java
@@ -7,6 +7,7 @@ public class Application {
 
     public static void main(String[] args) {
 
+        //calling display method
         Application application = new Application();
         application.display();
     }
